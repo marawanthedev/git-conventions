1faf1515e0dbd2883401f70c30fc8536ef848065
ahmadelraqab <amjrr99@gmail.com>
2024-05-28 20:27:25 +0300

    asdasd: sjldklsdmf #123123

--- setup-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the path to the Git hooks directory
-hooks_dir=".githooks"
-
-# Create symbolic links for each hook script in the .githooks directory
-for hook in $(ls $hooks_dir); do
-    ln -sf ../../$hooks_dir/$hook .git/hooks/$hook
-done
